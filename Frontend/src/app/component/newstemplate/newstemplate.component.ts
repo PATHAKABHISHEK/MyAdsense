ff555ad7e35316b5cca88a58c4c573a868e28d25
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-04-10 18:41:15 +0530

    stripe integerated with application

--- Frontend/src/app/component/newstemplate/newstemplate.component.ts
+++ Frontend/src/app/component/newstemplate/newstemplate.component.ts
@@ -1,28 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-newstemplate',
-  templateUrl: './newstemplate.component.html',
-  styleUrls: ['./newstemplate.component.css']
+  selector: "app-newstemplate",
+  templateUrl: "./newstemplate.component.html",
+  styleUrls: ["./newstemplate.component.css"],
 })
 export class NewstemplateComponent implements OnInit {
-
-  content="";
-  price=30;
+  content = "";
+  price = 30;
   count;
-  clicked=false;
-  constructor() { }
+  clicked = false;
+  constructor() {}
 
-  ngOnInit() {
-  }
-  WordCount(str) { 
+  ngOnInit() {}
+  WordCount(str) {
     return str.split(" ").length;
   }
-  onCalculate(){
+  onCalculate() {
     console.log(this.WordCount(this.content));
-    this.count=this.WordCount(this.content)*this.price;
-    this.clicked=true;
+    this.count = this.WordCount(this.content) * this.price;
+    this.clicked = true;
   }
- 
 
+  pay() {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: "pk_test_gEc03eRh7OBt0TpEUhacPJBz00n1JaNHUi",
+      locale: "auto",
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token.id);
+      },
+    });
+
+    handler.open({
+      name: "MyAdsense",
+      description: "Pay And Publish Ad",
+      amount: 2000,
+    });
+  }
 }
